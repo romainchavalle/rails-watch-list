4e3a245a1dfd1dda998a356a631cca3b31669f4a
romain chavalle <romain.chavalle@gmail.com>
2024-05-24 11:21:28 +0200

    exo1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'lists/index'
+  get 'lists/show'
+  get 'lists/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +11,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create, :destroy]
+  end
+
 end
